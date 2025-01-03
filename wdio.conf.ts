e6f3b5c9e319de42fa0b76a01cfd1620eb30629c
Usama Jalal <usama.jalal@arbisoft.com>
2025-01-03 18:48:59 +0500

    upd

--- wdio.conf.ts
+++ wdio.conf.ts
@@ -1,5 +1,5 @@
 import * as os from "os";
-import allure from "allure-commandline";
+//import allure from "allure-commandline";
 export const config: WebdriverIO.Config = {
   //
   // ====================
@@ -303,24 +303,21 @@ export const config: WebdriverIO.Config = {
    * @param {Array.<Object>} capabilities list of capabilities details
    * @param {<Object>} results object containing test results
    */
-  onComplete: function () {
-    const reportError = new Error("Could not generate Allure report");
-    const generation = allure(["generate", "allure-results", "--clean"]);
-    return new Promise((resolve, reject) => {
-      const generationTimeout = setTimeout(() => reject(reportError), 5000);
+  //   onComplete: function () {
+  //     const reportError = new Error("Could not generate Allure report");
+  //     const generation = allure(["generate", "allure-results", "--clean"]);
+  //     return new Promise((resolve, reject) => {
+  //       const generationTimeout = setTimeout(() => reject(reportError), 5000);
 
-      generation.on("exit", function (exitCode: number) {
-        clearTimeout(generationTimeout);
-
-        if (exitCode !== 0) {
-          return reject(reportError);
-        }
-
-        console.log("Allure report successfully generated");
-        resolve(undefined);
-      });
-    });
-  },
+  //       generation.on("exit", function (exitCode: number) {
+  //         clearTimeout(generationTimeout);
+  //         if (exitCode !== 0) {
+  //           return reject(reportError);
+  //         }
+  //         console.log("Allure report successfully generated");
+  //       });
+  //     });
+  //   },
   /**
    * Gets executed when a refresh happens.
    * @param {string} oldSessionId session ID of the old session
